10ef521d97b7fc6a7f00dc0215e96f637efde050
Acidic <ares.kittell@gmail.com>
2026-08-06 16:07:59 -0700

    Fixed errors, simple enemy1 created that shoots bullets at player

--- Assets/Enemies/LinearShoot.cs
+++ Assets/Enemies/LinearShoot.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 public class LinearShoot : MonoBehaviour
 {
     private Rigidbody2D rb;
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     // need to add collision destruction
     public void Shoot(float x, float y, float spd)
     {
-        Vector2 dir = new Vector2(x, y).normalized;
-        rb.linearVelocity = dir * spd;
+        Vector2 dir = new Vector2(x, y);
+        rb.linearVelocity = dir.normalized * spd;
     }
 
     public void Shoot(Vector2 dir, float spd)
--- Assets/Enemies/ShootAction.cs
+++ Assets/Enemies/ShootAction.cs
@@ -6,17 +6,18 @@ using Unity.Properties;
 using UnityEditor.UI;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Shoot", story: "Shoot [projectile] at [player] from [self]", category: "Action", id: "f15dbb84f643de9b958775943ff4d7d9")]
+[NodeDescription(name: "Shoot", story: "Shoot [projectile] at speed [speed] from [self]", category: "Action", id: "f15dbb84f643de9b958775943ff4d7d9")]
 public partial class ShootAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Projectile;
-    [SerializeReference] public BlackboardVariable<GameObject> Player;
+    [SerializeReference] public BlackboardVariable<float> Speed;
     [SerializeReference] public BlackboardVariable<GameObject> Self;
     protected override Status OnStart()
     {
-        GameObject proj = GameObject.Instantiate(Projectile, Self.transform.position, Self.transform.rotation);
-        LinearShoot shoot_script = proj.GetComponent<LinearShoot>();
-        shoot_script.Shoot(Player.transform.position.x, Player.transform.position.y);
+        GameObject proj = GameObject.Instantiate(Projectile.Value, Self.Value.transform.position, Self.Value.transform.rotation);
+        LinearShoot shootScript = proj.GetComponent<LinearShoot>();
+        GameObject player = GameObject.FindWithTag("Player");
+        shootScript.Shoot(player.transform.position.x - Self.Value.transform.position.x, player.transform.position.y - Self.Value.transform.position.y, Speed);
         return Status.Success;
     }
 }
