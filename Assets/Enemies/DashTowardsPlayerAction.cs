ad5b519ff378b60d855b7e788e1e66782820354f
Acidic <ares.kittell@gmail.com>
2026-08-06 22:11:27 -0700

    Added enemy4, which dashes at player and shoots projectiles

---
+++ Assets/Enemies/DashTowardsPlayerAction.cs
@@ -0,0 +1,42 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "DashTowardsPlayer", story: "[Self] dashes towards player at speed [speed] for [duration] seconds", category: "Action", id: "028ff48c8bc4a1e60f25308724910fab")]
+public partial class DashTowardsPlayerAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<float> Speed;
+    [SerializeReference] public BlackboardVariable<float> Duration;
+    private Rigidbody2D rb;
+    private float t;
+
+    protected override Status OnStart()
+    {
+        rb = Self.Value.GetComponent<Rigidbody2D>();
+        GameObject Target = GameObject.FindWithTag("Player");
+        Vector2 vel_dir = Target.transform.position - Self.Value.transform.position;
+        rb.linearVelocity = vel_dir.normalized * Speed;
+        t = 0;
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        t += Time.deltaTime;
+        if(t > Duration) 
+        {
+            rb.linearVelocity = new Vector2(0f, 0f);
+            return Status.Success;
+        }
+        return Status.Running;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
