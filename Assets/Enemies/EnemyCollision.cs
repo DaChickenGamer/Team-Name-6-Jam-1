63a1db9b227d0d60194c5b1699c1fd023f97849e
Acidic <ares.kittell@gmail.com>
2026-08-08 15:35:43 -0700

    Fixed enemy contact damage

--- Assets/Enemies/EnemyCollision.cs
+++ Assets/Enemies/EnemyCollision.cs
@@ -4,7 +4,7 @@ public class EnemyCollision : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int contactDamage = 1;
-    void OrEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
