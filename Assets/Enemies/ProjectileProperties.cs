b53293491a06343637bd8f0cd562e09e4f37002a
ChickenGamer <chickencodes@gmail.com>
2026-08-09 17:34:08 -0400

    Enemy File Organization

--- Assets/Enemies/ProjectileProperties.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class ProjectileProperties : MonoBehaviour
-{
-    public int damage = 0;
-}
---
+++ Assets/Scripts/Enemy/EnemyStun.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EnemyStun : MonoBehaviour
+{
+    public bool isStunned;
+    void Awake()
+    {
+        isStunned = false;
+    }
+}
