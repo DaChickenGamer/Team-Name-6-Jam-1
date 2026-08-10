b2b36454d5269e0e6265d6bdc5baa1becc5ee134
Acidic <ares.kittell@gmail.com>
2026-08-09 19:48:16 -0700

    Bullets now rotate

--- Assets/Scripts/Projectile/LinearShoot.cs
+++ Assets/Scripts/Projectile/LinearShoot.cs
@@ -22,8 +22,10 @@ public class LinearShoot : MonoBehaviour
     }
     public void Shoot(float x, float y, float spd)
     {
-        Vector2 dir = new Vector2(x, y);
-        rb.linearVelocity = dir.normalized * spd;
+        Vector2 dir = new Vector2(x, y).normalized;
+        double angle = Math.Atan2((double)dir.y, (double)dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, (float)angle);
+        rb.linearVelocity = dir * spd;
     }
 
     public void Shoot(Vector2 dir, float spd)
