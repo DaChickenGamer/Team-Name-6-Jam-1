d7bcf5faf1603bb713d0f0c0c3a378dddb1a869c
Trqnx <williamleslie60@gmail.com>
2026-08-10 15:01:03 -0500

    Fixed stingray behavior

---
+++ Assets/Scripts/Enemy/StingrayEntry.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StingrayEntry : MonoBehaviour
+{
+    [SerializeField] AudioClip roarClip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SoundFXManager.Instance.PlaySoundFXClip(roarClip, transform, 0.2f);
+    }
+}
