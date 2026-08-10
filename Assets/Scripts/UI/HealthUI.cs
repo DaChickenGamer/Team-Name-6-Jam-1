16192159c6956d83533766445febb73bb7f67fa5
ChickenGamer <chickencodes@gmail.com>
2026-08-09 21:01:20 -0400

    working health ui

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -4,56 +4,27 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
     public GameObject healthBar;
     public List<Image> heartContainer;
     
     public Sprite fullHeart;
     public Sprite emptyHeart;
-    
-    public int lastFilledHeartIndex;
-    
-    private void Start()
+
+    public void RedrawUI(int hearts, int maxHearts)
     {
-        for (int i = 0; i < playerHealth.maxHealth; i++)
+        foreach (var heart in heartContainer)
         {
-            AddHeart();
+            Destroy(heart.gameObject);
         }
         
-    }
-
-    public void AddHeart()
-    {
-        GameObject HeartInstance = Instantiate(healthBar, this.transform);
-        heartContainer.Add(HeartInstance.GetComponent<Image>());
-        lastFilledHeartIndex++;
-    }
-    
-    public void RemoveHeart()
-    {
-        if (heartContainer.Count == 0) return;
-
-        int lastIndex = heartContainer.Count - 1;
-        Image heart = heartContainer[lastIndex];
-        heartContainer.RemoveAt(lastIndex);
-        if (heart)
-            Destroy(heart.gameObject);
-
-        if (lastFilledHeartIndex > heartContainer.Count)
-            lastFilledHeartIndex = heartContainer.Count;
-    }
-
-    public void FillHeart()
-    {
-        if (lastFilledHeartIndex >= heartContainer.Count) return;
-        heartContainer[lastFilledHeartIndex].sprite = fullHeart;
-        lastFilledHeartIndex++;
-    }
-
-    public void UnfillHeart()
-    {
-        if (lastFilledHeartIndex <= 0) return;
-        lastFilledHeartIndex--;
-        heartContainer[lastFilledHeartIndex].sprite = emptyHeart;
+        heartContainer.Clear();
+        
+        for (int i = 0; i < maxHearts; i++)
+        {
+            GameObject heartInstance = Instantiate(healthBar, transform);
+            Image heartImage = heartInstance.GetComponent<Image>();
+            heartImage.sprite = i < hearts ? fullHeart : emptyHeart;
+            heartContainer.Add(heartImage);
+        }
     }
 }
